baf9f3449d380b4e2ba4700677b34aab34fa8bfe
Vi <Vitheaxolotl@gmail.com>
2026-07-14 14:28:09 -0400

    ??

--- src/map.js
+++ src/map.js
@@ -410,6 +410,10 @@ function addCharacter(character, update)
     
     if(!tokenImg.includes("custom-")){char[0].src = window.imgs["tokens"][tokenImg];} else{char[0].src = window.wholeCustom[tokenImg]["src"]; char[0].classList.add("customImg");}
     char[0].id = character["id"];
+    char[0].style.width = "56px";
+    char[0].style.height = "56px";
+    char[0].style.borderRadius = "50px"; 
+    char[0].style.objectFit = "cover";
     
     char[1].src = window.imgs["borders"][character["border"]];
     char[1].id = character["border"];
